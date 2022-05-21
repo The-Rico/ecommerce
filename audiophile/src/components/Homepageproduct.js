0d439b5741d6c8ab249845db693031039aa306ec
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-05-21 04:03:42 -0700

    Hero is complete

--- audiophile/src/components/Homepageproduct.js
+++ audiophile/src/components/Homepageproduct.js
@@ -1,3 +1,20 @@
 export default function Homepageproduct() {
-  return <p>test</p>;
+  return (
+    <div className='homePageHeroContainer'>
+      <div className='homePageHeroContent'>
+        <div className='homeProductDescription'>
+          <p className='heroNewProductParagraph'>NEW PRODUCT</p>
+          <h1>XX99 MARK II HEADPHONES</h1>
+          <p className='heroProductInfo'>
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <button className='seeProductButton'>See product</button>
+        </div>
+        <div className='homeProductImage '>
+          <img className='heroImage' src='./images/image-hero.jpg'></img>
+        </div>
+      </div>
+    </div>
+  );
 }
