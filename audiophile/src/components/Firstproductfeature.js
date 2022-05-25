cba4c4e7e1fb5e8c772fe08514fbf563b696a4f4
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-05-24 17:36:56 -0700

    zx9 section complete

--- audiophile/src/components/Firstproductfeature.js
+++ audiophile/src/components/Firstproductfeature.js
@@ -1,3 +1,26 @@
 export default function Firstproductfeature() {
-  return <div className='header'></div>;
+  return (
+    <div className='zx9FeatureContainer'>
+      <div className='zx9Feature'>
+        <img
+          alt='spiral svg'
+          className='zx9SpiralImage'
+          src='./images/pattern-circles.svg'
+        ></img>
+        <img
+          alt='zx9 speaker'
+          className='zx9HomeImage'
+          src='./images/image-speaker-zx9.png'
+        ></img>
+        <div className='zx9FeatureText'>
+          <h1>ZX9 SPEAKER</h1>
+          <p className='heroProductInfo'>
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <button className='zx9ProductButton'>SEE PRODUCT</button>
+        </div>
+      </div>
+    </div>
+  );
 }
